722e92186e46b1a909b6851dc5e0409f317d5c47
Tianchen Xu <TianchenX@outlook.com>
2019-04-02 08:22:39 +0800

    Screenshots updated.

--- VoxelizerX/Content/Shaders/PSSimple.hlsl
+++ VoxelizerX/Content/Shaders/PSSimple.hlsl
@@ -26,10 +26,9 @@ min16float4 main(PSIn input) : SV_TARGET
 	const min16float lightAmtCube = saturate(dot(input.NrmCube.xyz, lightDir));
 	const min16float lightAmt = lerp(lightAmtMesh, lightAmtCube, NORNAL_BLEND);
 
-	const min16float ambientMesh = lerp(0.25, 0.5, saturate(input.NrmMesh.y + 0.5));
-	const min16float ambientCube = lerp(0.25, 0.5, saturate(input.NrmCube.y + 0.5));
+	const min16float ambientMesh = lerp(0.2, 0.5, saturate(input.NrmMesh.y + 0.5));
+	const min16float ambientCube = lerp(0.2, 0.5, saturate(input.NrmCube.y + 0.5));
 	const min16float ambient = lerp(ambientMesh, ambientCube, NORNAL_BLEND);
 
-	return min16float4((lightAmt + 0.75 * ambient * (input.NrmMesh.w + 0.2)).xxx, 1.0);
-	//return min16float4(input.Nrm * 0.5 + 0.5, 1.0);
+	return min16float4((lightAmt * 0.8 + ambient * input.NrmMesh.w).xxx, 1.0);
 }
