457fc23f86ebcf46789634190a208c03be59bc1d
StarsX <X-Universe@outlook.com>
2022-06-25 21:37:27 +0800

    XUSG update for adding swapchain and present flags

--- VoxelizerX/VoxelizerX.cpp
+++ VoxelizerX/VoxelizerX.cpp
@@ -84,7 +84,7 @@ void VoxelizerX::LoadPipeline()
 	}
 #endif
 
-	com_ptr<IDXGIFactory4> factory;
+	com_ptr<IDXGIFactory5> factory;
 	ThrowIfFailed(CreateDXGIFactory2(dxgiFactoryFlags, IID_PPV_ARGS(&factory)));
 
 	DXGI_ADAPTER_DESC1 dxgiAdapterDesc;
@@ -112,7 +112,7 @@ void VoxelizerX::LoadPipeline()
 	// Describe and create the swap chain.
 	m_swapChain = SwapChain::MakeUnique();
 	XUSG_N_RETURN(m_swapChain->Create(factory.get(), Win32Application::GetHwnd(), m_commandQueue.get(),
-		Voxelizer::FrameCount, m_width, m_height, Format::R8G8B8A8_UNORM), ThrowIfFailed(E_FAIL));
+		Voxelizer::FrameCount, m_width, m_height, Format::R8G8B8A8_UNORM, SwapChainFlag::ALLOW_TEARING), ThrowIfFailed(E_FAIL));
 
 	// This sample does not support fullscreen transitions.
 	ThrowIfFailed(factory->MakeWindowAssociation(Win32Application::GetHwnd(), DXGI_MWA_NO_ALT_ENTER));
@@ -219,7 +219,7 @@ void VoxelizerX::OnRender()
 	m_commandQueue->ExecuteCommandList(m_commandList.get());
 
 	// Present the frame.
-	XUSG_N_RETURN(m_swapChain->Present(0, 0), ThrowIfFailed(E_FAIL));
+	XUSG_N_RETURN(m_swapChain->Present(0, PresentFlag::ALLOW_TEARING), ThrowIfFailed(E_FAIL));
 
 	MoveToNextFrame();
 }
